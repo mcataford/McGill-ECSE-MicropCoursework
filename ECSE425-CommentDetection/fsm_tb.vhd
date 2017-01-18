f1af742f3dcbcc65af547e3e6d596261e68c7162
mcataford <c.marcandre@gmail.com>
2017-01-17 23:42:31 -0500

    Set up array for test cases.

--- ECSE425-CommentDetection/fsm_tb.vhd
+++ ECSE425-CommentDetection/fsm_tb.vhd
@@ -23,6 +23,15 @@ CONSTANT SLASH_CHARACTER : std_logic_vector(7 downto 0) := "00101111";
 CONSTANT STAR_CHARACTER : std_logic_vector(7 downto 0) := "00101010";
 CONSTANT NEW_LINE_CHARACTER : std_logic_vector(7 downto 0) := "00001010";
 
+-- Test cases are implemented using arrays of 8-bit vectors.
+-- Arrays of 16 characters are used as it is assumed that
+-- 16-bits is enough to have a variety of transitions
+-- representative of all useful test cases.
+type test_case is array (0 to 15) of std_logic_vector(7 downto 0);
+
+-- Case 1: "//comment\n code"
+signal test_case1: test_case := ("00101111","00101111","01100011","01101111","01101101","01101101","01100101","01101110","01110100","00001010","00100000","01100011","01101111","01100100","01100101","00001010");
+
 BEGIN
 dut: comments_fsm
 PORT MAP(clk, s_reset, s_input, s_output);
@@ -38,18 +47,19 @@ END PROCESS;
 
 --TODO: Thoroughly test your FSM
 stim_process: PROCESS
+
 BEGIN
 	REPORT "Example case, reading a meaningless character";
-	s_input <= "00101111";
-	WAIT FOR 1 * clk_period;
-    s_input <= "00101111";
-	WAIT FOR 1 * clk_period;
-    s_input <= "01011000";
-	WAIT FOR 1 * clk_period;
-    s_input <= "00001010";
-	WAIT FOR 1 * clk_period;
-	ASSERT (s_output = '0') REPORT "When reading a meaningless character, the output should be '0'" SEVERITY ERROR;
+	--s_input <= "00101111";
+	--WAIT FOR 1 * clk_period;
+    ASSERT (s_output = '0') REPORT "When reading a meaningless character, the output should be '0'" SEVERITY ERROR;
 	REPORT "_______________________";
+
+    report "Test case 1: h// tes*\n";
+    for i in test_case1' range loop
+      s_input <= test_case1(i);
+      wait for 1 * clk_period;
+    end loop;
 
 	WAIT;
 END PROCESS stim_process;
