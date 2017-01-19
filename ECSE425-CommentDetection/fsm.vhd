d063b13871746868889dd251e122b968fd52981d
mcataford <c.marcandre@gmail.com>
2017-01-19 00:03:39 -0500

    Submission commit.

--- ECSE425-CommentDetection/fsm.vhd
+++ ECSE425-CommentDetection/fsm.vhd
@@ -19,13 +19,24 @@ constant STAR_CHARACTER : std_logic_vector(7 downto 0) := "00101010";
 constant NEW_LINE_CHARACTER : std_logic_vector(7 downto 0) := "00001010";
 
 -- fsm_state has four possible states.
+-- State A: Detection of the first part of the comment opening (/) Output=0
+-- State B: Detection of the second part of the comment opening (* or /) Output=0
+-- State C: Detection of the first part of the comment closing (* or \n) Output=1
+-- State D: Detection of the second part of the comment closing (/) Output=1
+
+--It is assumed that if a comment is opened, the FSM will remain in State C or D until a full closing sequence is detected.
 signal fsm_state: std_logic_vector(1 downto 0) := "00";
+
+-- Comment type, 0 if initiated by //, 1 for /*
+signal comment_type: std_logic := '0';
 
 begin
 
 process (clk, reset)
 begin
 
+  -- Since unspecified, an asynchronous reset has been implemented.
+  -- This decision is supported by the inclusion of the reset signal in the sensitivity list.
   if reset = '1' then
 
   	fsm_state <= "00";
@@ -51,9 +62,17 @@ begin
 	-- State B: Detect the second char. of the start of a comment, output 0.
     when "01" =>
       -- Second stage of starting a comment is either / or *
-      if (input = SLASH_CHARACTER) or (input = STAR_CHARACTER) then
-		-- If detected, go the state C, otherwise remain B.
+      if input = SLASH_CHARACTER then
+		  -- If detected, go the state C, otherwise back to A.
         fsm_state <= "10";
+        comment_type <= '0';
+
+      elsif input = STAR_CHARACTER then
+        fsm_state <= "10";
+        comment_type <= '1';
+
+      else
+        fsm_state <= "00";
 
       end if;
 
@@ -62,12 +81,12 @@ begin
 	-- State C: Detect the end of a comment, output 1 since presently inside a comment.
 	when "10" =>
       -- If \n, the comment ends now, back to state A for comment opening detection.
-	  if input = NEW_LINE_CHARACTER then
+	  if (input = NEW_LINE_CHARACTER) and (comment_type = '0') then
 
-		fsm_state <= "00";
+		  fsm_state <= "00";
 
 	  -- If *, start of the end-comment sequence */, go to state D.
-	  elsif input = STAR_CHARACTER then
+	  elsif (input = STAR_CHARACTER) and (comment_type = '1') then
 
 	    fsm_state <= "11";
 
