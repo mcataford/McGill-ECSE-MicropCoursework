a69c970b452688ca23bda426ac7fa53bc4f50b4f
mcataford <c.marcandre@gmail.com>
2017-01-22 19:55:10 -0500

    Basic five stages, ready for preliminary tests.

--- ECSE425-Pipelining/pipeline.vhd
+++ ECSE425-Pipelining/pipeline.vhd
@@ -11,10 +11,61 @@ end pipeline;
 
 architecture behavioral of pipeline is
 
+--We define all of the signals used as current values of the ops.
+signal current_op1, current_op2, current_op3, current_op4, current_op5: integer;
+
+--We also define the signals that will be the 'next' value meant for the next cycle.
+signal next_op1, next_op2, next_op3, next_op4, next_op5: integer;
+
+--To force signals to be delayed until the right pipeline stage.
+signal a_1, a_2, a_3, e_1, e_2, e_3, c_1, c_2, d_1, d_2, op2_1, op2_2, op2_3, op2_4, op3_1, op3_2 : integer;
+
 begin
 -- todo: complete this
 process (clk)
 begin
+
+	if rising_edge(clk) then
+		current_op1 <= next_op1;
+		current_op2 <= next_op2;
+		current_op3 <= next_op3;
+		current_op4 <= next_op4;
+		current_op5 <= next_op5;
+
+		op1 <= current_op1;
+		op2 <= current_op2;
+		op2_1 <= current_op2;
+		op2_2 <= op2_1;
+		op2_3 <= op2_2;
+		op2_4 <= op2_3;
+		op3 <= current_op3;
+		op3_1 <= current_op3;
+		op3_2 <= op3_1;
+		op4 <= current_op4;
+		op5 <= current_op5;
+	
+		a_1 <= a;
+		a_2 <= a_1;
+		a_3 <= a_2;
+		e_1 <= e;
+		e_2 <= e_1;
+		e_3 <= e_2;
+		c_1 <= c;
+		c_2 <= c_1;
+		d_1 <= d;
+		d_2 <= d_1;
+
+	end if;
+
 end process;
+
+next_op1 <= a + b;
+next_op2 <= current_op1 * 42;
+next_op3 <= c_2 * d_2;
+next_op4 <= a_3 - e_3;
+next_op5 <= op3_2 * current_op4;
+final_output <= op2_4 - current_op5;
+
+
 
 end behavioral;
